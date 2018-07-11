0816c1f2534515601b59f3e4859e744be485a521
Robert DeBeer <Robert.DeBeer@gmail.com>
2018-07-11 17:46:12 -0500

    creates complete order button, css, & functionality

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -18,7 +18,19 @@ class BurgerBuilder extends Component {
       cheese: 0,
       meat: 0
     },
-    totalPrice: 1.25
+    totalPrice: 1.25,
+    purchasable: false
+  }
+
+  updatePurchaseState (ingredients) {
+    const sum = Object.keys(ingredients).map(igKey => {
+      return ingredients[igKey]
+    }).reduce((sum, el) => {
+      return sum + el;
+    }, 0);
+    this.setState({
+      purchasable: sum > 0
+    });
   }
 
   addIngredientHandler = (type) => {
@@ -35,6 +47,7 @@ class BurgerBuilder extends Component {
       totalPrice: newPrice, 
       ingredients: updatedIngredients
     });
+    this.updatePurchaseState(updatedIngredients);
   }
 
   removeIngredientHandler = (type) => {
@@ -54,6 +67,7 @@ class BurgerBuilder extends Component {
       totalPrice: newPrice, 
       ingredients: updatedIngredients
     });
+    this.updatePurchaseState(updatedIngredients);
   }
 
   render () {
@@ -70,7 +84,8 @@ class BurgerBuilder extends Component {
           ingredientAdded={this.addIngredientHandler}
           ingredientRemoved={this.removeIngredientHandler} 
           disabled={disabledInfo} 
-          price={this.state.totalPrice}/>
+          price={this.state.totalPrice}
+          purchasable={this.state.purchasable} />
       </Fragment>
     );
   }
