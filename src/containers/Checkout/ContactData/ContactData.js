0d5a9fd31ad653d9bf6452437be49afbfa5aafb8
Robert DeBeer <Robert.DeBeer@gmail.com>
2018-07-17 14:32:10 -0500

    creates a custom input component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 
 import axios from '../../../axios-orders';
 import Button from '../../../components/UI/Button/Button';
-import Spinner from '../../../components/UI/Spinner/Spinner'
+import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 import classes from './ContactData.css';
 
 class ContactData extends Component {
@@ -52,11 +53,29 @@ class ContactData extends Component {
   render () {
     let form = (
       <form>
-        <input className={classes.Input} type='text' name='name' placeholder="Name" />
-        <input className={classes.Input} type='text' name='email' placeholder="Email" />
-        <input className={classes.Input} type='text' name='street' placeholder="Street" />
-        <input className={classes.Input} type='text' name='postal' placeholder="Postal Code" />
-        <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>
+        <Input 
+          inputtype='input' 
+          type='text' 
+          name='name' 
+          placeholder="Name" />
+        <Input 
+          inputtype='input' 
+          type='text' 
+          name='email' 
+          placeholder="Email" />
+        <Input 
+          inputtype='input' 
+          type='text' 
+          name='street' 
+          placeholder="Street" />
+        <Input 
+          inputtype='input' 
+          type='text' 
+          name='postal' 
+          placeholder="Postal Code" />
+        <Button 
+          btnType='Success' 
+          clicked={this.orderHandler}>ORDER</Button>
       </form>
     );
     if (this.state.loading) {
