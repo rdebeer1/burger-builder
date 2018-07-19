5589596cc0489809fb85bdb15731d1dcf723e7fa
Robert DeBeer <Robert.DeBeer@gmail.com>
2018-07-19 17:26:50 -0500

    refactors updatePurchaseState w/ global state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -12,7 +12,6 @@ import * as actionTypes from '../../store/actions';
 
 class BurgerBuilder extends Component {
   state = {
-    purchasable: false,
     purchasing: false,
     loading: false,
     error: null,
@@ -38,9 +37,7 @@ class BurgerBuilder extends Component {
     }).reduce((sum, el) => {
       return sum + el;
     }, 0);
-    this.setState({
-      purchasable: sum > 0
-    });
+    return sum > 0
   }
 
   purchaseHandler = () => {
@@ -86,7 +83,7 @@ class BurgerBuilder extends Component {
             ingredientRemoved={this.props.onIngredientRemoved} 
             disabled={disabledInfo} 
             price={this.props.price}
-            purchasable={this.state.purchasable} 
+            purchasable={this.updatePurchaseState(this.props.ings)} 
             ordered={this.purchaseHandler} />
         </Fragment>
       );
